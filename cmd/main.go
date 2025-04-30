8cdab6bd16393671554c4ec039cb729a2798fffe
abuzbekb <abukaozbekbay@gmail.com>
2025-04-30 11:42:59 +0500

    removing gofumpt

--- cmd/main.go
+++ cmd/main.go
@@ -26,7 +26,7 @@ func main() {
     http.HandleFunc("DELETE /orders/", handlers.DeleteOrder(dbConn))
     http.HandleFunc("GET /orders/", handlers.GetOrderByID(dbConn))
     http.HandleFunc("PUT /orders/", handlers.UpdateOrderByID(dbConn))
-    http.HandleFunc("POST /orders/close/", handlers.CloseOrder(dbConn))go install mvdan.cc/gofumpt@latest
+    http.HandleFunc("POST /orders/close/", handlers.CloseOrder(dbConn))
 
     // Inventory routes
     http.HandleFunc("GET /inventory", handlers.GetInventoryItems(dbConn))
